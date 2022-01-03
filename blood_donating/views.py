df3117a3708045c694498e9f6a9f99a307179204
Yahia Labib <85199984+yahialabeeb@users.noreply.github.com>
2022-01-03 11:30:33 +0200

    Revert "add posts api"

--- blood_donating/views.py
+++ blood_donating/views.py
@@ -1,27 +1,3 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView , ListAPIView,RetrieveUpdateDestroyAPIView
-from .models import Post
-from .serializer import PostSerializer
-from rest_framework import permissions
 
 # Create your views here.
-
-class PostViewSet(CreateAPIView):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GetViewSet(ListAPIView):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    serializer_class = PostSerializer
-    queryset =Post.objects.all().order_by("-time")
-    # permission_classes = [permissions.IsAuthenticated]
-
-class UpdateDestroyViewSet(RetrieveUpdateDestroyAPIView):
-    serializer_class = PostSerializer
-    queryset =Post.objects.all().order_by("-time")
-    # permission_classes = [permissions.IsAuthenticated]
