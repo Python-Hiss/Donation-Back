6622a1b0b38bfb5c954b5eae678d634f3a3f716a
yaseen1998 <yasseenkouthe@eng.hu.edu>
2022-01-05 18:43:54 +0200

    data

--- accounts/models.py
+++ accounts/models.py
@@ -17,7 +17,7 @@ class CustomUser(AbstractUser):
     )
     phone_number = models.CharField(max_length=20,blank=True, help_text='Contact phone number')
     address = models.ForeignKey(Address,on_delete=models.CASCADE,default=1,blank=True,null=True,related_name='tracks')
-    image = models.ImageField(upload_to = 'image',default='../uploads/image/profileiamge.jpg',blank=True,null=True)
+    image = models.ImageField(upload_to = 'image',default='../files/image/profileiamge.jpg',blank=True,null=True)
     roles = models.CharField(max_length=50, choices = ROLES, null=True,blank=True)
     def __str__(self):
         return self.username
